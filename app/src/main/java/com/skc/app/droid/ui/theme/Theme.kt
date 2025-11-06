b9f6129beebdb36f39b688eaf0f29fb6421fcb9e
Javi <8519394+rtomyj@users.noreply.github.com>
2025-11-05 19:58:27 -0600

    Fixing warnings

--- app/src/main/java/com/skc/app/droid/ui/theme/Theme.kt
+++ app/src/main/java/com/skc/app/droid/ui/theme/Theme.kt
@@ -41,11 +41,10 @@ fun SKCTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
