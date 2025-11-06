aafa86d3ca1ce347482790d9ea15194d47a243a7
Javi <8519394+rtomyj@users.noreply.github.com>
2025-11-06 12:36:20 -0600

    Added coil for remote image async loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,11 +54,16 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.coil.compose)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
