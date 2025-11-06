a6ed183e9a5a2382999262fcb0d8061a0260e901
Javi <8519394+rtomyj@users.noreply.github.com>
2025-11-06 12:37:02 -0600

    Formatting

--- app/src/main/java/com/skc/app/droid/util/DatesUtil.kt
+++ app/src/main/java/com/skc/app/droid/util/DatesUtil.kt
@@ -2,8 +2,6 @@ package com.skc.app.droid.util
 
 import java.time.format.DateTimeFormatter
 
-
-
 class DatesUtil {
     companion object {
         val SKC_DATE_FORMAT: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
