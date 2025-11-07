4030662280b312fc11166af42ec210ed45fcd74b
Javi <8519394+rtomyj@users.noreply.github.com>
2025-11-06 21:35:13 -0600

    Added CotD

--- app/src/main/java/com/skc/app/droid/MainActivity.kt
+++ app/src/main/java/com/skc/app/droid/MainActivity.kt
@@ -4,21 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.skc.app.droid.ui.home.CardOfTheDay
 import com.skc.app.droid.ui.home.DBStats
 import com.skc.app.droid.ui.theme.SKCTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SKCTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    DBStats(modifier = Modifier.padding(innerPadding))
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 5.dp),
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.spacedBy(40.dp)
+                    ) {
+                        DBStats()
+                        CardOfTheDay()
+                    }
                 }
             }
         }
