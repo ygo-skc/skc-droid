37bc3fc7f1789a7d7a89c28dc9d7362d07f7efda
Javi <8519394+rtomyj@users.noreply.github.com>
2025-11-08 16:43:21 -0600

    Updated client config

--- app/src/main/java/com/skc/app/droid/service/Client.kt
+++ app/src/main/java/com/skc/app/droid/service/Client.kt
@@ -1,13 +1,36 @@
 package com.skc.app.droid.service
 
+import okhttp3.ConnectionPool
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val httpClient = OkHttpClient.Builder().addInterceptor { chain ->
+    val original = chain.request()
+
+    val requestBuilder = original.newBuilder()
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .header("Connection", "keep-alive")
+        .header("User-Agent", "SKCDroid")
+        .header("CLIENT_ID", "SKCDroid")
+//        .header("Accept-Encoding", "gzip, deflate")
+
+    val request = requestBuilder.build()
+    chain.proceed(request)
+}
+    .connectTimeout(1, TimeUnit.SECONDS)
+    .readTimeout(3, TimeUnit.SECONDS)
+    .connectionPool(
+        ConnectionPool(8, 30, TimeUnit.SECONDS)
+    )
+
 
 fun getSKCAPIClient(): SKCService {
     return Retrofit.Builder().baseUrl("https://skc-ygo-api.com")
         .addConverterFactory(GsonConverterFactory.create())
-        .client(OkHttpClient())
+        .client(httpClient.build())
         .build()
         .create(SKCService::class.java)
 }
@@ -15,7 +38,7 @@ fun getSKCAPIClient(): SKCService {
 fun getSuggestionEngineClient(): SuggestionEngineService {
     return Retrofit.Builder().baseUrl("https://suggestions.skc-ygo-api.com")
         .addConverterFactory(GsonConverterFactory.create())
-        .client(OkHttpClient())
+        .client(httpClient.build())
         .build()
         .create(SuggestionEngineService::class.java)
 }
