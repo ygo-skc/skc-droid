b90ff4c5fa9cca624235686f09d801d8525568d1
Javi <8519394+rtomyj@users.noreply.github.com>
2025-11-11 20:04:17 -0600

    Added tab bar

--- app/src/main/java/com/skc/app/droid/MainActivity.kt
+++ app/src/main/java/com/skc/app/droid/MainActivity.kt
@@ -8,11 +8,23 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Whatshot
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.skc.app.droid.ui.home.CardOfTheDay
@@ -31,9 +43,40 @@ class MainActivity : ComponentActivity() {
                 LaunchedEffect("Home View Model") {
                     homeViewModel.fetchData()
                 }
+
+                val tabs = listOf(
+                    Icons.Default.Home,
+                    Icons.Filled.Whatshot
+                )
+                var selectedIndex by remember { mutableIntStateOf(0) }
+
                 Scaffold(
                     modifier = Modifier
-                        .fillMaxSize()
+                        .fillMaxSize(),
+                    bottomBar = {
+                        NavigationBar(
+                            modifier = Modifier.height(72.dp),
+                        ) {
+                            tabs.forEachIndexed { index, icon ->
+                                NavigationBarItem(
+                                    selected = selectedIndex == index,
+                                    onClick = { selectedIndex = index },
+                                    icon = {
+                                        Icon(
+                                            imageVector = icon,
+                                            contentDescription = null
+                                        )
+                                    },
+                                    label = null,
+                                    alwaysShowLabel = false,
+                                    colors = NavigationBarItemDefaults.colors(
+                                        unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                        indicatorColor = MaterialTheme.colorScheme.primaryContainer
+                                    )
+                                )
+                            }
+                        }
+                    }
                 ) { innerPadding ->
                     Column(
                         modifier = Modifier
