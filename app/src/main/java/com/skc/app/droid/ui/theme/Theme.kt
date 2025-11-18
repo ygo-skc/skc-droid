8109e25256bba2f0e307f418b722934bfb0ba50d
Javi <8519394+rtomyj@users.noreply.github.com>
2025-11-18 16:47:36 -0600

    Update Theme.kt

--- app/src/main/java/com/skc/app/droid/ui/theme/Theme.kt
+++ app/src/main/java/com/skc/app/droid/ui/theme/Theme.kt
@@ -3,15 +3,17 @@ package com.skc.app.droid.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = PrimaryDark,
 
     secondary = SecondaryDark,
-    tertiary = TertiaryDark
     secondaryContainer = Color(0xFFff8f00),
     onSecondaryContainer = Color(0xFFeceff1),
 
