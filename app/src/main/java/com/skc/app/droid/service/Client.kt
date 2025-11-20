c703205928d85d4e0fb76a322de4446237f54ed1
Javi <8519394+rtomyj@users.noreply.github.com>
2025-11-19 21:26:29 -0600

    Updating configs

--- app/src/main/java/com/skc/app/droid/service/Client.kt
+++ app/src/main/java/com/skc/app/droid/service/Client.kt
@@ -2,20 +2,20 @@ package com.skc.app.droid.service
 
 import okhttp3.ConnectionPool
 import okhttp3.OkHttpClient
+import okhttp3.Protocol
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+
+private val converterFactory = GsonConverterFactory.create()
 
 private val httpClient = OkHttpClient.Builder().addInterceptor { chain ->
     val original = chain.request()
 
     val requestBuilder = original.newBuilder()
-        .header("Accept", "application/json")
-        .header("Content-Type", "application/json")
         .header("Connection", "keep-alive")
         .header("User-Agent", "SKCDroid")
         .header("CLIENT_ID", "SKCDroid")
-//        .header("Accept-Encoding", "gzip, deflate")
 
     val request = requestBuilder.build()
     chain.proceed(request)
@@ -23,32 +23,29 @@ private val httpClient = OkHttpClient.Builder().addInterceptor { chain ->
     .connectTimeout(1, TimeUnit.SECONDS)
     .readTimeout(3, TimeUnit.SECONDS)
     .connectionPool(
-        ConnectionPool(8, 30, TimeUnit.SECONDS)
+        ConnectionPool(8, 5, TimeUnit.MINUTES)
     )
+    .protocols(listOf(Protocol.HTTP_2, Protocol.HTTP_1_1))
+    .build()
 
 
-fun getSKCAPIClient(): SKCAPIService {
-    return Retrofit.Builder()
-        .commonConfigs("https://skc-ygo-api.com")
-        .create(SKCAPIService::class.java)
-}
+fun getSKCAPIClient(): SKCAPIService = Retrofit.Builder()
+    .commonConfigs("https://skc-ygo-api.com")
+    .create(SKCAPIService::class.java)
 
-fun getSuggestionEngineClient(): SuggestionEngineService {
-    return Retrofit.Builder()
-        .commonConfigs("https://suggestions.skc-ygo-api.com")
-        .create(SuggestionEngineService::class.java)
-}
 
-fun getHeartAPIClient(): HeartAPIService {
-    return Retrofit.Builder()
-        .commonConfigs("https://heart-api.com")
-        .create(HeartAPIService::class.java)
-}
+fun getSuggestionEngineClient(): SuggestionEngineService = Retrofit.Builder()
+    .commonConfigs("https://suggestions.skc-ygo-api.com")
+    .create(SuggestionEngineService::class.java)
 
-private fun Retrofit.Builder.commonConfigs(baseURL: String): Retrofit {
-    return this
-        .baseUrl(baseURL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(httpClient.build())
-        .build()
-}
+
+fun getHeartAPIClient(): HeartAPIService = Retrofit.Builder()
+    .commonConfigs("https://heart-api.com")
+    .create(HeartAPIService::class.java)
+
+
+private fun Retrofit.Builder.commonConfigs(baseURL: String): Retrofit = this
+    .baseUrl(baseURL)
+    .addConverterFactory(converterFactory)
+    .client(httpClient)
+    .build()
