94fb527dbf40d4af317166c65424653401eecbc6
Javi <8519394+rtomyj@users.noreply.github.com>
2025-11-22 21:31:25 -0600

    Updated UI

--- app/src/main/java/com/skc/app/droid/ui/card/ygo/CardProductInfo.kt
+++ app/src/main/java/com/skc/app/droid/ui/card/ygo/CardProductInfo.kt
@@ -1,18 +1,23 @@
 package com.skc.app.droid.ui.card.ygo
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Diamond
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SuggestionChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.skc.app.droid.model.ProductItem
 
@@ -50,18 +55,27 @@ private fun Rarities(rarityCount: Map<String, Int>, cardName: String) {
 
         FlowRow(
             horizontalArrangement = Arrangement.spacedBy(6.dp),
-            verticalArrangement = Arrangement.spacedBy(0.dp)
+            verticalArrangement = Arrangement.spacedBy(6.dp)
         ) {
             rarityCount.forEach {
-                SuggestionChip(
-                    onClick = {},
-                    label = {
-                        Text(
-                            text = "${it.key} (${it.value})",
-                            style = MaterialTheme.typography.labelMedium
-                        )
-                    },
-                )
+                Row(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(size = 10.dp))
+                        .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+                        .padding(vertical = 6.dp, horizontal = 10.dp),
+                    horizontalArrangement = Arrangement.spacedBy(5.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = it.value.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        fontWeight = FontWeight.Medium
+                    )
+                    Text(
+                        text = it.key,
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
             }
         }
     }
